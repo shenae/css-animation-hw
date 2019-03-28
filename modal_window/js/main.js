185a581b37803b816b8fa30ce0c68dcffb68b10c
Shenae Simmons <shhnae@gmail.com>
2019-03-28 18:09:59 -0400

    finished modal box

--- modal_window/js/main.js
+++ modal_window/js/main.js
@@ -1,1 +1,20 @@
 // Write your pseudo code here! HAPPY CODING!!
+//<button type="button" id="showModal">Click for a Good Time</button>
+// <div class="modal-background">
+//     <div class="modal-content">
+//       <h3>Hi, I'm a Modal</h3>  
+//       <p>Use me for content, photos, forms, etc.</p>
+//       <button id="close">Close</button>
+// let modal = document.querySelector('.modal-content');
+// let buttonOpen = document.getElementById('showModal');
+// let buttonClose = document.getElementById('close');
+document.getElementById('showModal').addEventListener('click',function(){
+	document.querySelector('.modal-background').style.display = "block";
+	});
+
+document.getElementById('close').addEventListener('click',function(){
+	document.querySelector('.modal-background').style.display = "none";
+	});
+
+
+
