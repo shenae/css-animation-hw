6ab3bb8f22ee1b3f267483c63966c7369f250ae7
Shenae Simmons <shhnae@gmail.com>
2019-03-28 21:14:44 -0400

    completed all 3

--- sidebar_menu/js/main.js
+++ sidebar_menu/js/main.js
@@ -1,1 +1,17 @@
 // Write your pseudo code here! HAPPY CODING! :) 
+//Step #3 (in main.js): Toggle the .active class on the .sidebar when the hamburger icon is clicked.
+let hamburger = document.querySelector('.hamburger'); 
+let navMenu = document.querySelector('.sidebar');
+let close = document.querySelector('.close');
+
+
+hamburger.addEventListener('click', function(){
+	navMenu.classList.toggle('active');
+
+
+
+});
+close.addEventListener('click', function(){
+	navMenu.classList.toggle('active');
+})
+
--- sticky_nav/js/main.js
+++ sticky_nav/js/main.js
@@ -3,7 +3,24 @@ $(window).on('scroll', function () {
 	var distanceScrolled = $(window).scrollTop();
 
 	console.log('The distance scrolled is: ' + distanceScrolled);
-
+let stickyNav = document.querySelector('.main-nav');
 	// Step 2: Write Rest of JS here
 
+if(distanceScrolled > 540) {
+	stickyNav.style.position = "fixed";
+} else if(distanceScrolled < 540) {
+	stickyNav.style.position = "static";
+}
 });
+//notes from da interwebs
+//$("li.test").offset().top how far down the menu is
+//.scrollTop() will always be 0 on mobile phones.
+//Use $(element).offset().top and add height of existing fixed elements on the page to it to make it more accurate.
+
+// $('div#something').scroll(function () {
+//     if ($(this).scrollTop() > 200) {
+//         $(this).addClass('foo');
+//     } else {
+//         $(this).removeClass('foo');
+//     }
+// });
